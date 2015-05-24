636bf58a6bff969743de90d56649d07c94c1c52f
merrik <frenkdave@gmail.com>
2015-05-24 03:21:56 +0300

    JSDoc comments

--- js/room.js
+++ js/room.js
@@ -1,4 +1,10 @@
-// Constructor
+/**
+ * Представляет комнату чата
+ * 
+ * @constructor
+ * 
+ * @param  string name
+ */
 function Room(name) {
   this.name = name;
   this.users = {};
@@ -6,21 +12,38 @@ function Room(name) {
   this.numUsers = 0;
 }
 
+/**
+ * Возвращает пользователей комнаты
+ * @return object
+ */
 Room.prototype.getUsers = function() {
   return this.users;
 }
 
+/**
+ * Вовзвращает количество пользователей в комнате
+ * @return number
+ */
 Room.prototype.getUsersCount = function() {
   return this.numUsers;
 }
 
+/**
+ * Добавляет сообщения в историю
+ * @param  string user
+ * @param  string data
+ */
 Room.prototype.addToHistory = function(user, data) {
-  this.history.push({message:data, user: user});
+  this.history.push({ message:data, user: user });
   if (this.history.length > 30) {
     this.history.shift();
   }
 }
 
+/**
+ * Отправляет историю комнаты в сокет
+ * @param  socket
+ */
 Room.prototype.sendHistory = function(socket) {
   if (this.history.length > 0) {
     this.history.forEach(function(data) {
