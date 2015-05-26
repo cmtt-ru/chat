4411c3d6bcce77c10a9ce31e051abee19fb329a8
Ilya Chekalskiy <ilya@chekalskiy.ru>
2015-05-26 16:00:26 +0300

    bugfix

--- public/main.js
+++ public/main.js
@@ -141,7 +141,9 @@ $(function() {
 
   // --------------------------------------------------------------
 
-  var socket = io();
+  var socket = io('//' + document.location.host, {
+    reconnection: true
+  });
 
   // Socket events
   socket.on('connect', function() {
@@ -222,17 +224,3 @@ $(function() {
     alert('Access denied');
   });
 });
-
-String.prototype.getHashCode = function() {
-    var hash = 0;
-    if (this.length == 0) return hash;
-    for (var i = 0; i < this.length; i++) {
-        hash = this.charCodeAt(i) + ((hash << 5) - hash);
-        hash = hash & hash; // Convert to 32bit integer
-    }
-    return hash;
-};
-Number.prototype.intToHSL = function() {
-    var shortened = this % 360;
-    return "hsl(" + shortened + ",100%,30%)";
-};
